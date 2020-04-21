e96db60bda3a24a1132b43da01fba0704557b8bc
Ranilson <ranilson.analista2020@hotmail.com>
2020-04-20 23:07:32 -0300

    Adicionei Console.Reline no final do projeto!

--- NovaRedeSocial/Program.cs
+++ NovaRedeSocial/Program.cs
@@ -12,25 +12,27 @@ namespace NovaRedeSocial
             //instanciando o post
             Post p1 = new Post(
                 DateTime.Parse("20/04/2020 15:52:55"),
-                "Traveling to New Zealand",
-                "I'm going to visit this wonderful country!",12);
-            
+                "Traveling to New Zealand",  //Viajar para a Nova Zelândia
+                "I'm going to visit this wonderful country!",12);  //Eu vou visitar este país maravilhoso!
+
             p1.AddComment(c1);
             p1.AddComment(c2);
 
-            Comment c3 = new Comment("Good night");  //Tenha uma boa viagem!
-            Comment c4 = new Comment("May the Force be with you"); //Uau isso é incrível!
+            Comment c3 = new Comment("Good night");  //Boa Noite!
+            Comment c4 = new Comment("May the Force be with you"); //Que a força esteja com você
             //instanciando o post
             Post p2 = new Post(
                 DateTime.Parse("21/04/2020 12:52:55"),
-                "Good nigth guys",
-                "See you tomorrow", 22);
+                "Good nigth guys",  //Boa noite galera
+                "See you tomorrow", 22);  //Te vejo amanhã
 
             p2.AddComment(c3);
             p2.AddComment(c4);
 
             Console.WriteLine(p1);
             Console.WriteLine(p2);
+
+            Console.ReadLine();
         }
 
     }
